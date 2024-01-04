429649e9faf41b1ac37fd8fc18ec34529519a1e9
Seidgy Amano <seidgy@gmail.com>
2024-01-04 18:05:19 -0300

    Forms do mailchimp

--- components/formNewsletter.vue
+++ components/formNewsletter.vue
@@ -1,15 +1,15 @@
 <template>
-    <form class="form | form-parceiro">
-       <!-- <h3 class="h4">Assine nossa newsletter</h3>
+    <form class="form | form-parceiro" action="https://prevlaw.us14.list-manage.com/subscribe/post?u=18e80cddaa76b0befc4d3a492&amp;id=30550c0d42&amp;f_id=00aa88e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+        <h3 class="h4">Assine nossa newsletter</h3>
         <div class="form__group">
-            <label for="email" class="form__label | hidden">E-mail</label>
-            <input type="email" id="email" name="email" class="form__input" placeholder="Coloque o seu melhor e-mail" />
+            <label for="mce-EMAIL" class="form__label | hidden">E-mail</label>
+            <input type="email" id="mce-EMAIL" name="EMAIL" required="" class="form__input" placeholder="Coloque o seu melhor e-mail" />
         </div>
         <div class="form__group">
-            <input type="checkbox" id="termos" class="form__input" name="termos" />
-            <label for="termos" class="form__label">Eu aceito se inscrever e concordo com as políticas de privacidade do site Prevlaw</label>
+            <input type="checkbox" required="" id="mce-MMERGE50" class="form__input" name="MMERGE5" value="Aceito" />
+            <label for="mce-MMERGE50" class="form__label">Eu aceito se inscrever e concordo com as políticas de privacidade do site Prevlaw</label>
         </div>
-        <button type="submit" class="button button--primary button--full">Assine Agora</button>-->
+        <button type="submit" class="button button--primary button--full">Assine Agora</button>
     </form>
 </template>
 
