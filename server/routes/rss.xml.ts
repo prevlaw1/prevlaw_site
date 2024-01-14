ba9cfe9d6a8890bded5da7ef56447b9ef7f3db1b
Seidgy Amano <seidgy@gmail.com>
2024-01-14 20:55:53 -0300

    Feed RSS

---
+++ server/routes/rss.xml.ts
@@ -0,0 +1,40 @@
+import { serverQueryContent } from '#content/server'
+import RSS from 'rss';
+
+export default defineEventHandler(async (event) => {
+    const feed = new RSS({
+      title: 'Prevlaw',
+      description: 'Feed RSS para o site Prevlaw',
+      site_url: 'https://www.prevlaw.com',
+      feed_url: `https://www.prevlaw.com/rss.xml`,
+    })
+    
+    
+    const docs = await serverQueryContent(event).sort({ date: -1 }).where({ _partial: false }).find()
+    
+    let blogPosts = docs.filter((doc) => doc?.type?.includes('noticias') || doc?.type?.includes('beneficios') || doc?.type?.includes('revisoes'))
+
+    
+    for (const doc of blogPosts) {
+    
+      feed.item({
+    
+        title: doc.title ?? '-',
+    
+        url: `https://www.prevlaw.com${ doc.type === 'noticias' ? '/noticias' : doc.type === 'beneficios' ? '/beneficios' : '/revisoes' }/${doc.slug }`,
+    
+        date: doc.date,
+    
+        description: doc.description,
+    
+      })
+    
+    }
+    
+    
+    const feedString = feed.xml({ indent: true })
+    
+    event.res.setHeader('content-type', 'text/xml')
+    
+    event.res.end(feedString)
+});
