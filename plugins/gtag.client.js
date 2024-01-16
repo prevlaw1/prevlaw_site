f0db2c31e580bc7134fcfa95718e4c9fb74e12d2
Seidgy Amano <seidgy@gmail.com>
2024-01-16 15:17:32 -0300

    Analytics config

--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -1,21 +1,20 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    const { gtagId } = useRuntimeConfig().public;
-    
-    function gtag() {
-      window.dataLayer.push(arguments);
-    }
-    
-    window.dataLayer = window.dataLayer || [];
-    
-    gtag("js", new Date());
-    gtag("config", gtagId);
-    
-    useHead({
-      script: [
-        {
-          src: `https://www.googletagmanager.com/gtag/js?id=G-RF1EHT6VV2`,
-          async: true,
-        },
-      ],
-    });
+  
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  
+  window.dataLayer = window.dataLayer || [];
+  
+  gtag("js", new Date());
+  gtag("config", 'G-RF1EHT6VV2');
+  
+  useHead({
+    script: [
+      {
+        src: `https://www.googletagmanager.com/gtag/js?id=AW-11469171212`,
+        async: true,
+      },
+    ],
   });
+});
