af81ba499f818247ff62b1cb4310a52c5695b3a6
Seidgy Amano <seidgy@gmail.com>
2024-03-19 10:21:58 -0300

    Fix

--- directus/publicacao.js
+++ directus/publicacao.js
@@ -3,17 +3,19 @@ import rimraf from "rimraf";
 import common from "./common.js";
 import { DOMParser } from 'xmldom';
 
+let files = await common.getDirectusAssets();
+
 const objectContructor = async (dir, fs) => {
   let peticoes = await common.getDirectusData("publicacao");
   
   peticoes.forEach(async (item, num) => {
     let i = { ...item };
     i.slug = common.slugify(item.titulo);
-    i.capa = await common.getImage(item.capa.id);
+    i.capa = await getImageURL(item.capa.id);
     i.type = 'noticias'
     i.categoriaSlug = common.slugify(item.categoria.titulo)
     i.autorSlug = common.slugify(item.autor.nome)
-    i.autor.picture = await common.getImage(item.autor.picture)
+    i.autor.picture = await getImageURL(item.autor.picture)
     let firstParagraph = '';
     const parser = new DOMParser();
     const htmlDoc = parser.parseFromString(i.conteudo, 'text/html');
@@ -70,6 +72,14 @@ const getPublicacoes = async () => {
       }
     });
   }
+}
+
+
+const getImageURL = async (imageId) => {
+  const image = files.find(file => file.id === imageId);
+  let ext = image.filename_download.split('.').pop();
+  return `/images/directus/${imageId}.${ext}`;
+  //return `${ process.env.DIRECTUS_IMAGE_BASE_URL }/assets/${ imageId }`;
 }
 
 export default getPublicacoes
